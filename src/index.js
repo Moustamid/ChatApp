495281ed4ec81dcfc0d3e2ff463c8da8f93f0e88
Moustamid <karim1992moustamid@gmail.com>
2021-03-06 12:44:33 -0600

    adding socket.io

--- src/index.js
+++ src/index.js
@@ -2,26 +2,36 @@
 const path = require('path');
 //-npm
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
 const chalk = require('chalk');
 
 // SECTION:
+
 const app = express();
+const server = http.createServer(app);
+//- our server now support web socket.io
+const io = socketio(server);
 
 // SECTION:
 
 const publicDirectoryPath = path.join(__dirname, '../public');
+
+// SECTION:
+
 app.use(express.static(publicDirectoryPath));
 
 // SECTION:
 
-app.get('', (req, res) => {
-  res.render('index');
+io.on('connection', () => {
+  console.log(chalk.hex('#8c14fc').bold(` New WebSocket connection`));
 });
 
 // SECTION:
-const PORT = 3000;
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
   console.log(
     chalk.hex('#26a65b').bold(`Express Server running on port ${PORT} ...👩‍💻`)
   );
